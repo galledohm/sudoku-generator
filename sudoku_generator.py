5aa2f6d42460d2d0e71a079be5b4a5929f81474f
galledohm <adrigalle.san@gmail.com>
2025-12-16 18:19:16 +0100

    feat: Added support to import sudokus from code and generate sudokus for final users

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -5,7 +5,7 @@ recursive backtracking to fill a 9x9 grid following standard Sudoku rules.
 """
 
 import random
-
+import copy
 
 class SudokuBoard:
     """A Sudoku board generator and validator using backtracking algorithms.
@@ -14,9 +14,18 @@ class SudokuBoard:
     validate board states, and manipulate 3x3 subsquares.
     """
 
-    def __init__(self, board=None):
+    def __init__(self, board=None, code=None):
+        _exclusive_args = [board,code]
+        _args_count = sum(1 for arg in _exclusive_args if arg is not None)
+        self.board = []
+        self._solution = []
+
+        if _args_count > 1:
+            raise ValueError("Only one of the following arguments can be specified: 'board' or 'code'")
         if board:
             self.board = board
+        elif code:
+            self.board = self.code_to_board(code)
         else:
             self.__reset_board()
 
@@ -44,16 +53,16 @@ class SudokuBoard:
         for i in range(3):
             self.board[row_start + i][col_start:col_start + 3] = subsquare[i]
 
-    def generate_subsquare(self):
+    def _generate_subsquare(self):
         """Generate a random valid 3x3 subsquare with numbers 1-9."""
         numbers = list(range(1, 10))
         random.shuffle(numbers)
         return [numbers[i*3:(i+1)*3] for i in range(3)]
 
-    def generate_diagonal(self):
+    def _generate_diagonal(self):
         """Generate the three diagonal subsquares (0, 4, 8)."""
         for i in range(3):
-            self.set_subsquare(i*4, self.generate_subsquare())
+            self.set_subsquare(i*4, self._generate_subsquare())
 
     def check_subsquare(self, num: int):
         """ Check if a subsquare contains no duplicates """
@@ -75,7 +84,7 @@ class SudokuBoard:
         filled_numbers = [num for num in column if num != 0]
         return (len(filled_numbers) == len(set(filled_numbers)))
 
-    def fill_remaining(self, row: int, col: int):
+    def _fill_remaining(self, row: int, col: int):
         """ Placeholder for a backtracking algorithm to fill the remaining cells. """
         # End condition
         if row == 9:
@@ -83,43 +92,70 @@ class SudokuBoard:
 
         # Next row (final column reached)
         if col == 9:
-            return self.fill_remaining(row + 1, 0)
+            return self._fill_remaining(row + 1, 0)
 
         if self.board[row][col] != 0:
-            return self.fill_remaining(row, col + 1)
+            return self._fill_remaining(row, col + 1)
 
         for i in range(1,10):
             self.board[row][col] = i
             if (self.check_subsquare(self.get_num_subsquare(row, col)) and
                     self.check_row(row) and self.check_column(col)):
-                if self.fill_remaining(row, col + 1):
+                if self._fill_remaining(row, col + 1):
                     return True
             # Always reset the cell when backtracking, regardless of validation result
             self.board[row][col] = 0
 
         return False
 
-    def generate_full_board(self):
+    def _generate_full_board(self):
         """Generate a complete valid Sudoku board."""
-        self.generate_diagonal()
-        return self.fill_remaining(0, 0)
+        self._generate_diagonal()
+        return self._fill_remaining(0, 0)
+
+    def _remove_cells(self, sudoku):
+        """Delete 40 cells"""
+        rows = [random.randint(0,8) for _ in range(40)]
+        cols = [random.randint(0,8) for _ in range(40)]
+        for i in range(40):
+            sudoku[rows[i]][cols[i]] = 0
+        return sudoku
+
+    def generate_sudoku(self):
+        """Generate a sudoku"""
+        if self._generate_full_board():
+            self._solution = copy.deepcopy(self.board)
+        else:
+            raise ValueError("Sudoku could not be generated")
+        return self._remove_cells(self.board)
 
     def board_to_code(self):
         """Convert the board to an 81-character string representation."""
         code = []
-        for row in self.board:
+        for row in self.board: # type: ignore
             for cell in row:
                 code.append(cell)
         return ''.join(map(str, code))
 
+    def code_to_board(self, code):
+        """Convert the 81-character string code to a proper board."""
+        code_list = list(map(int,code))
+        aux_board = []
+        for i in range(0, len(code_list), 9):
+            aux_board.append(code_list[i:i+9])
+        return aux_board
+
 if __name__ == "__main__":
     # Example usage
-    sudoku = SudokuBoard()
-    print(sudoku.board_to_code())
-    if sudoku.generate_full_board():
-        print("SUCCESS! Full board generated:")
-        for board_row in sudoku.board:
+    # boardcode = "569217834327458169148396572415923786782165943693874251831749625954682317276531498"
+    # sudoku = SudokuBoard(code=boardcode)
+    # for board_row in sudoku.board: # type: ignore
+    #         print(board_row)
+    sudoku_board = SudokuBoard()
+    final_sudoku = sudoku_board.generate_sudoku()
+    if final_sudoku:
+        for board_row in final_sudoku: # type: ignore
             print(board_row)
-        print(sudoku.board_to_code())
+        print(f'\n\nSudoku code: {sudoku_board.board_to_code()}')
     else:
         print("Failed to generate board")
